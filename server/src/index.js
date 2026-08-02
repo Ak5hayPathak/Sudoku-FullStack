064cf7ab9059d507e55de061b3497671c8a93cf8
Akshay Pathak <akshaypathak0821@gmail.com>
2026-08-02 12:43:55 +0530

    connected database and created the server

---
+++ server/src/index.js
@@ -0,0 +1,64 @@
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("ERR: ", error);
+      throw error;
+    });
+
+    app.listen(port, () => {
+      console.log(`Server listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGODB Connection Failed!! - ${err}`);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// import express from 'express';
+// const app = express()
+// ;(async () => {
+
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//         app.on('error', (error) => {
+//             console.error("ERR: ", error);
+//             throw error;
+//         });
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port: ${process.env.PORT}`);
+//         })
+
+//     }catch(error){
+//         console.error("ERROR: " , error);
+//         throw error;
+
+//     }
+
+// })();
